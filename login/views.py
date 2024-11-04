87ad194f3bffad3604b8b1d52c2a6f945cdffff5
LucasTHOMAS18 <lucas.thomas2108@gmail.com>
2024-11-04 19:09:52 +0100

    Static and base.html

--- login/views.py
+++ login/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 class CustomLoginView(LoginView):
-    template_name = 'login.html'  # Nom du template pour la page de connexion
+    template_name = 'login.html'
