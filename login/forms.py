ca996aeb8ce9feba124d5fb05b3ae9bbc04cbe1d
Amin ELMELLOUKI <amin.el-mellouki@etu.univ-orleans.fr>
2024-11-05 16:37:56 +0100

    mise a jour de login

---
+++ login/forms.py
@@ -0,0 +1,8 @@
+# forms.py
+from django import forms
+
+class PasswordOnlyForm(forms.Form):
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Entrez votre mot de passe'}),
+        label="Mot de passe"
+    )
