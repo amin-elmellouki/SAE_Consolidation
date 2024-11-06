4b6e24c4c12224d7d4ecb1f15f55eff702f1a5dd
Amin ELMELLOUKI <amin.el-mellouki@etu.univ-orleans.fr>
2024-11-06 10:34:28 +0100

    réalisation du code html pour la page de login

--- login/forms.py
+++ login/forms.py
@@ -4,5 +4,4 @@ from django import forms
 class PasswordOnlyForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Entrez votre mot de passe'}),
-        label="Mot de passe"
     )
