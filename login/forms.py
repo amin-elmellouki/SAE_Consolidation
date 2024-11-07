6ed131036344c66bc52feb20ba3cf0c358a5e5e2
Fbouam <fahed.bouamrani@etu.univ-orleans.com>
2024-11-07 11:13:05 +0100

    refactorisation du fichier views.py en plusieur autres fichiers

--- login/forms.py
+++ login/forms.py
@@ -1,7 +1,9 @@
-# forms.py
 from django import forms
+from django.core.exceptions import ValidationError
+from .validators import password_strength
 
 class PasswordOnlyForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Entrez votre mot de passe'}),
+        validators=[password_strength],
     )
--- login/views.py
+++ login/views.py
@@ -3,28 +3,18 @@ from django.shortcuts import render, redirect
 from .forms import PasswordOnlyForm
 from django.contrib import messages
 from django.contrib.auth.hashers import check_password
-from django.core.exceptions import ValidationError
-from django.contrib.auth.password_validation import validate_password
-import re as regex
 
 def password_login_view(request):
     if request.method == 'POST':
         form = PasswordOnlyForm(request.POST)
         if form.is_valid():
             entered_password = form.cleaned_data['password']
-            try:
-                validate_password(entered_password)
-                if not regex.search(r'[!@#$%^&*(),.?":{}|<>]', entered_password):
-                    raise ValidationError("Le mot de passe doit contenir au moins un caractère spécial (!@#$%^&*(),.?\":{}|<>).")
-            except ValidationError as e:
-                form.add_error('password', e)
+            if check_password(entered_password, settings.UNIQUE_PASSWORD_HASH):
+                request.session['authenticated'] = True
+                return redirect('home')
             else:
-                if check_password(entered_password, settings.UNIQUE_PASSWORD_HASH):
-                    request.session['authenticated'] = True
-                    return redirect('home')
-                else:
-                    messages.error(request, "Mot de passe incorrect.")
+                messages.error(request, "Mot de passe incorrect.")
     else:
         form = PasswordOnlyForm()
-    
+
     return render(request, 'login.html', {'form': form})
