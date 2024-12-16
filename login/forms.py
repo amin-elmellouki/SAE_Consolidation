f630049ce8506f6ff58796eb6f30db9828b56b4a
Amin ELMELLOUKI <amin.el-mellouki@etu.univ-orleans.fr>
2024-12-16 16:40:34 +0100

    ajout de validation de mdp en temps reel, amelioraiton du design de la page de login

--- login/forms.py
+++ login/forms.py
@@ -1,22 +1,42 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
-from .validators import password_strength
 
 class UsernamePasswordLoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
-            'placeholder': 'Entrez votre nom d\'utilisateur',
-            'style': 'width: 185px;'
+            'placeholder': "Entrez votre nom d'utilisateur",
+            'style': 'width: 220px;'
         }),
-        label=_("")
+        label=""
     )
 
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Entrez votre mot de passe',
-            'style': 'width: 185px;'
+            'style': 'width: 220px;'
         }),
-        validators=[password_strength],
-        label=_("")
+        label=""
     )
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise forms.ValidationError(
+                _("La combinaison nom d'utilisateur et mot de passe est invalide."),
+                code='invalid_login'
+            )
+        # Vérification supplémentaire : permet aux superutilisateurs de bypass les règles
+        if user.is_superuser:
+            self.cleaned_data['user'] = user
+            return self.cleaned_data
+
+        self.cleaned_data['user'] = user
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.cleaned_data.get('user')
