00ebb5ee74b96e132bfea946566843914651f2fe
LucasTHOMAS18 <maitre.renardowo@gmail.com>
2024-12-17 09:38:48 +0100

    Icons

--- bilan/views.py
+++ bilan/views.py
@@ -5,7 +5,6 @@ from home import models
 
 def bilan(request, date):
     bilan = list(models.get_bilan(date))
-    print(bilan[0])
     return render(request, 'bilan.html', {
         'bilans': bilan
     })
