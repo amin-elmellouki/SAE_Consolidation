b218dc27dde4d262988f97db04e05566690dae8d
ugodominguez <ugo.dominguez@etu.univ-orleans.fr>
2025-02-24 15:11:28 +0100

    add colors to bilan

--- bilan/static/color.js
+++ bilan/static/color.js
@@ -5,16 +5,16 @@ window.addEventListener('load', function () {
         const value = parseFloat(cell.getAttribute('data-note'));
         
         if (value >= 15) {
-            cell.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
+            cell.style.backgroundColor = 'rgba(0, 255, 0, 0.5)';
             cell.style.color = 'black';
         } else if (value >= 10) {
-            cell.style.backgroundColor = 'rgba(154, 205, 50, 0.2)';
+            cell.style.backgroundColor = 'rgba(154, 205, 50, 0.5)';
             cell.style.color = 'black';
         } else if (value >= 5) {
-            cell.style.backgroundColor = 'rgba(255, 165, 0, 0.2)';
+            cell.style.backgroundColor = 'rgba(255, 165, 0, 0.5)';
             cell.style.color = 'black';
         } else {
-            cell.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
+            cell.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
             cell.style.color = 'black';
         }
     });
