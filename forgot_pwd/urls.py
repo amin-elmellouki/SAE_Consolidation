ca0b65759af30624a2101608bf430a3ca7f599b9
Amin ELMELLOUKI <amin.el-mellouki@etu.univ-orleans.fr>
2025-02-28 12:00:00 +0100

    changement du style

--- forgot_pwd/urls.py
+++ forgot_pwd/urls.py
@@ -2,23 +2,23 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('forgot_password/', auth_views.PasswordResetView.as_view(
+    path('forgot-password/', auth_views.PasswordResetView.as_view(
          template_name='forgot_password.html',
          email_template_name='password_reset_email.html',
          subject_template_name='password_reset_subject.txt',
-         success_url='/password_reset_done/'
+         success_url='/password-reset-done/'
     ), name='password_reset'),
 
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(
          template_name='password_reset_done.html'
     ), name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
          template_name='password_reset_confirm.html',
-         success_url='/password_reset_complete/'
+         success_url='/password-reset-complete/'
     ), name='password_reset_confirm'),
 
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
          template_name='password_reset_complete.html'
     ), name='password_reset_complete'),
 ]
